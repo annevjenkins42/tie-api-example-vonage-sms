b789f379ad13a354f08d4948bf4428fab25d4d75
AnneJ <73593442+annevjenkins42@users.noreply.github.com>
2025-03-11 07:13:12 +0000

    Update server.js

--- server.js
+++ server.js
@@ -75,10 +75,10 @@ function sendNexmoSMSMessage() {
 }
 
 
-// Send a SMS
+// Send a WhatsApp
 function sendSMS(phoneNumber, message) {
 	
-const from_number = '14157386102';
+const from_number = config.nexmoNumber || '14157386102';
 const to_number = phoneNumber;
 const data = JSON.stringify({
   "from": { "type": "whatsapp", "number": from_number },
@@ -91,8 +91,8 @@ const data = JSON.stringify({
   }
 });
 	const https = require('https');
-const user = 'd7a7df85';
-const password = '5LGQImJ4i8NxWfHW';
+const user = nexmoApiKey || 'd7a7df85';
+const password = nexmoApiSecret || '5LGQImJ4i8NxWfHW';
 const options = {
   hostname: 'messages-sandbox.nexmo.com',
   port: 443,
